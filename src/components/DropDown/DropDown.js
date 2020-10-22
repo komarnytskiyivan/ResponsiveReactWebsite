16044da4301a2c115c50f33c070d67aa72d20bd1
Ivan <komar.ivan.97@gmail.com>
2020-10-22 23:55:33 +0300

    Added dropdown comp

---
+++ src/components/DropDown/DropDown.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+import {Link} from 'react-router-dom';
+import { FaBars, FaTimes} from 'react-icons/fa';
+import {Button} from '../Button/Button';
+import './Navbar.css';
+import { IconContext } from 'react-icons/lib'
+function DropDown() {
+    return (
+        <div className="navbar-dropdown">
+        
+        </div>
+    )
+}
+
+export default DropDown
